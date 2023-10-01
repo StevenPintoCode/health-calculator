08f7fc995f3979ad55e01819604ddfc0059e6d89
Steven <stevenal.pinto@gmail.com>
2023-10-01 19:34:43 +0200

    Updated auditing with middleware.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {Request, Response} from 'express';
+import {NextFunction, Request, Response} from 'express';
 import {CalculateBMI, CalculateBMR, CalculateBodyFat} from './Services/CalculatorService';
 import {getErrorMessage} from './util';
 import {collections} from "./Services/MongoService";
@@ -12,31 +12,43 @@ const app = express();
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 const PORT = 8000;
+const CLASS_NAME = "index.ts"
 
-app.get('/calculator/bmi', async (req: Request, res: Response) => {
+app.use("/calculator",async (req: Request, res:Response, next:NextFunction) => {
+    const newAudit = {
+        _id: new ObjectId(),
+        timestamp: new Date(),
+        className: CLASS_NAME,
+        path: req.path,
+        type: undefined,
+        params: undefined,
+        response: undefined
+    } as Audit
+
+    const auditCall = await collections.audits?.insertOne(newAudit)
+    next()
+})
+
+app.get('/calculator/bmi', async (req: Request, res: Response, next: NextFunction) => {
 
     try {
+        console.log("Running bmi calc")
         const weight = parseInt(req?.query?.weight as string)
         const height = parseInt(req?.query?.height as string)
         const serviceResponse = CalculateBMI(weight, height)
 
-        //audit
-        const newAudit = {
-            _id: new ObjectId(),
-            timestamp: new Date(),
-            name: serviceResponse.type,
-            type: serviceResponse.type,
-            params: serviceResponse.input,
-            response: serviceResponse.result
-        } as Audit
-        const auditCall = await collections.audits?.insertOne(newAudit)
+        res.locals.finalResponse = serviceResponse
+        res.locals.path = "/calculator/bmi"
+
         res.status(200).json(serviceResponse)
+        next()
     } catch (error) {
         res.status(500).json({error: getErrorMessage(error)})
     }
 })
 
-app.get('/calculator/bmr', (req: Request, res: Response) => {
+
+app.get('/calculator/bmr', async (req: Request, res: Response, next: NextFunction) => {
 
     try {
         const weight = parseInt(req?.query?.weight as string)
@@ -44,13 +56,18 @@ app.get('/calculator/bmr', (req: Request, res: Response) => {
         const age = parseInt(req?.query?.age as string)
         const gender = req?.query?.gender as string
         const serviceResponse = CalculateBMR(weight, height, age, gender)
+
+        res.locals.finalResponse = serviceResponse
+        res.locals.path = "/calculator/bmi"
+
         res.status(200).json(serviceResponse)
+        next()
     } catch (error) {
         res.status(500).json({error: getErrorMessage(error)})
     }
 })
 
-app.get('/calculator/bodyfat', (req: Request, res: Response) => {
+app.get('/calculator/bodyfat', (req: Request, res: Response, next: NextFunction) => {
 
     try {
         const weight = parseInt(req?.query?.weight as string)
@@ -62,17 +79,36 @@ app.get('/calculator/bodyfat', (req: Request, res: Response) => {
         const neck = parseInt(req?.query?.neck as string)
 
         const serviceResponse = CalculateBodyFat(weight, height, age, gender, waist, hip, neck)
+        res.locals.finalResponse = serviceResponse
+        res.locals.path = "/calculator/bmi"
+
         res.status(200).json(serviceResponse)
+        next()
     } catch (error) {
         res.status(500).json({error: getErrorMessage(error)})
     }
 })
 
-app.get('/results', (req: Request, res: Response) => {
+app.use("/calculator", async (req: Request, res: Response, next: NextFunction) => {
+
+    const newAudit = {
+        _id: new ObjectId(),
+        timestamp: new Date(),
+        className: CLASS_NAME,
+        path: req.path,
+        type: res.locals?.finalResponse?.type,
+        params: res.locals?.finalResponse?.input,
+        response: res.locals?.finalResponse?.result
+    } as Audit
+
+    const auditCall = await collections.audits?.insertOne(newAudit)
+    next()
+})
+app.get('/results', (req: Request, res: Response, next: NextFunction) => {
     res.sendFile(path.join(__dirname, '/public/results.html'))
 })
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
--- src/models/auditUtil.ts
+++ src/models/auditUtil.ts
@@ -1,7 +1,14 @@
-import { ObjectId } from "mongodb"
+import {ObjectId} from "mongodb"
 import {CalculatorParams} from "../Factories/CalculatorFactory";
 
 export default class Audit {
-    constructor( public _id: ObjectId,public timestamp: Date, public name?: string, type?: string, params?: CalculatorParams, response?: string) {
+    constructor(public _id: ObjectId,
+                public timestamp: Date,
+                public className?: string,
+                public path?: string,
+                public type?: string,
+                public params?: CalculatorParams,
+                public response?: string,
+                public statusCode?: number) {
     }
 }
